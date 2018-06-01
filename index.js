260224945d8842fae8cefe1eed48e368c017eae3
scottyb <scottyb@scottybs-MacBook.local>
2018-05-31 21:33:33 -0700

    webpack dev server with react

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+
+// fs.read to read the data that's in the other file.
+
+class App extends React.Component {
+    
+    render () {
+        console.log('JK');
+        return (
+            <div>Realty M</div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById("scotttt"));
